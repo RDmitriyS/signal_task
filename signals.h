04bc792c9b3e2341911475dc8950528cc30865a0
RDmitriyS <sobakin_2001@mail.ru>
2020-10-31 02:25:29 +0300

    first commit

--- signals.h
+++ signals.h
@@ -1,27 +1,131 @@
 #pragma once
+
 #include <functional>
+#include <memory>
+#include <list>
 
-namespace signals
-{
+namespace signals {
 
-template <typename T>
-struct signal;
+    template<typename T>
+    struct signal;
 
-template <typename... Args>
-struct signal<void (Args...)>
-{
-    struct connection;
+    template<typename... Args>
+    struct signal<void(Args...)> {
 
-    signal();
+        using slot_t = std::function<void(Args...)>;
+        using slots_t = std::list<slot_t>;
 
-    signal(signal const&) = delete;
-    signal& operator=(signal const&) = delete;
+        struct connection {
+            connection() = default;
 
-    ~signal();
+            connection(signal *sig, typename slots_t::iterator it) :
+                sig(sig),
+                it(it),
+                destroyed(sig ? sig->destroyed : nullptr) {
+            }
 
-    connection connect(std::function<void (Args...)> slot) noexcept;
+            connection(connection && other) noexcept :
+                sig(other.sig),
+                it(std::move(other.it)),
+                destroyed(std::move(other.destroyed)) {
 
-    void operator()(Args...) const;
-};
+                other.sig = nullptr;
+                other.destroyed = nullptr;
+            }
 
+            connection &operator=(connection && rhs) noexcept {
+                if (this != &rhs) {
+                    disconnect();
+                    sig = rhs.sig;
+                    destroyed = rhs.destroyed;
+                    it = std::move(rhs.it);
+                    rhs.sig = nullptr;
+                    rhs.destroyed = nullptr;
+                }
+                return *this;
+            }
+
+            ~connection() {
+                disconnect();
+            }
+
+            void disconnect() {
+                if (sig && !*destroyed) {
+                    if (sig->inside_emit) {
+                        *it = slot_t();
+                    } else {
+                        sig->slots.erase(it);
+                    }
+
+                    sig = nullptr;
+                    destroyed = nullptr;
+                }
+            }
+
+        private:
+            signal *sig = nullptr;
+            std::shared_ptr<bool> destroyed;
+            typename slots_t::iterator it;
+        };
+
+        signal() : destroyed(std::make_shared<bool>(false)) {}
+
+        signal(signal const & other) :
+            destroyed(std::make_shared<bool>(false)),
+            slots(other.slots) {
+        }
+
+        signal &operator=(signal const & rhs) {
+            if (this != rhs) {
+                slots = rhs.slots;
+                destroyed = rhs.destroyed;
+            }
+
+            return *this;
+        }
+
+        ~signal() {
+            *destroyed = true;
+        }
+
+        connection connect(slot_t slot) {
+            slots.push_back(std::move(slot));
+            return connection(this, std::prev(slots.end()));
+        }
+
+        void operator()(Args... args) const {
+            ++inside_emit;
+            std::shared_ptr<bool> local_destroyed = destroyed;
+
+            try {
+                for (auto i = slots.begin(); i != slots.end(); ++i) {
+                    if (*i) {
+                        (*i)(std::forward<Args>(args)...);
+                        if (*local_destroyed) {
+                            return;
+                        }
+                    }
+                }
+            } catch (...) {
+                leave_emit();
+                throw;
+            }
+
+            leave_emit();
+        }
+
+    private:
+        void leave_emit() const noexcept {
+            if (--inside_emit != 0)
+                return;
+
+            for (auto i = slots.begin(); i != slots.end();) {
+                *i ? ++i : i = slots.erase(i);
+            }
+        }
+
+        mutable slots_t slots;
+        mutable size_t inside_emit = 0;
+        mutable std::shared_ptr<bool> destroyed;
+    };
 }
